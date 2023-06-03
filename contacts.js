7b55927fc2a11497f24df71c29a89b5aa3373ef8
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-06-03 20:53:40 +0200

    add comands

--- contacts.js
+++ contacts.js
@@ -10,42 +10,37 @@ async function listContacts() {
   try {
     const readContacts = await readFile(contactsPath);
     const listContacts = JSON.parse(readContacts);
-    console.log("Contacts list", listContacts);
+    return listContacts;
   } catch (error) {
     console.log(error);
   }
 }
-// listContacts();
 
 // FIND CONTACT BY ID
 async function getContactById(contactId) {
   try {
-    const readContacts = await readFile(contactsPath);
-    const listContacts = JSON.parse(readContacts);
-    const getContactById = listContacts.filter(
+    const contactsList = await listContacts();
+    const getContactById = contactsList.filter(
       (contact) => contact.id === contactId
     );
-    console.log("Contact has finded by id", getContactById);
+    return getContactById;
   } catch (error) {
     console.log(error);
   }
 }
-// getContactById("e6ywwRe4jcqxXfCZOj_1e");
 
 // --REMOVE CONTACT, NEW ARRAY
 async function removeContact(contactId) {
   try {
-    const readContacts = await readFile(contactsPath);
-    const listContacts = JSON.parse(readContacts);
-    const getNewArrayContacts = listContacts.filter(
+    const contactsList = await listContacts();
+    const getNewArrayContacts = contactsList.filter(
       (contact) => contact.id !== contactId
     );
-    console.log("New array after remove", getNewArrayContacts);
+    return getNewArrayContacts;
   } catch (error) {
     console.log(error);
   }
 }
-// removeContact("rsKkOQUi80UsgVPCcLZZW");
 
 // Add contacts - new array
 async function addContact(name, email, phone) {
@@ -56,16 +51,15 @@ async function addContact(name, email, phone) {
       email: email,
       phone: phone,
     };
-    const readContacts = await readFile(contactsPath);
-    const listContacts = JSON.parse(readContacts);
-    const newArray = [...listContacts, objectContacts];
 
-    await writeFile("newJson.json", JSON.stringify(newArray));
+    const contactsList = await listContacts();
+    const newArray = [...contactsList, objectContacts];
+    await writeFile(contactsPath, JSON.stringify(newArray));
   } catch (error) {
     console.log(error);
   }
 }
-// addContact("Lea Glorr", "lea123@gmail.com", "+380961234567");
+
 module.exports = {
   listContacts,
   getContactById,
