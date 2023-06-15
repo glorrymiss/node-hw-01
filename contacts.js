d195901a9e44972b6f307500b494aa36cd14bda1
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-06-15 18:16:24 +0200

    add wrapper trycatch

--- contacts.js
+++ contacts.js
@@ -2,69 +2,57 @@ const { readFile, writeFile } = require("fs").promises;
 const { nanoid } = require("nanoid");
 
 const path = require("path");
+const { wrapper } = require("./wrapper/wrapper");
 
 const contactsPath = path.join("db", "contacts.json");
 
 // GET LIST ALL CONTACTS
 async function listContacts() {
-  try {
-    const readContacts = await readFile(contactsPath);
-    const allContacts = JSON.parse(readContacts);
-    // console.table(allContacts);
-    return allContacts;
-  } catch (error) {
-    console.log(error);
-  }
+  const readContacts = await readFile(contactsPath);
+  const allContacts = JSON.parse(readContacts);
+  // console.table(allContacts);
+  return allContacts;
 }
 listContacts();
 // FIND CONTACT BY ID
 async function getContactById(contactId) {
-  try {
-    const contactsList = await listContacts();
-    const getContactById = contactsList.filter(
-      (contact) => contact.id === contactId
-    );
-    console.log(getContactById);
-  } catch (error) {
-    console.log(error);
-  }
+  const contactsList = await listContacts();
+  const getContactById = contactsList.filter(
+    (contact) => contact.id === contactId
+  );
+  console.log(getContactById);
 }
 
 // --REMOVE CONTACT, NEW ARRAY
 async function removeContact(contactId) {
-  try {
-    const contactsList = await listContacts();
-    const getNewArrayContacts = contactsList.filter(
-      (contact) => contact.id !== contactId
-    );
-    console.log(getNewArrayContacts);
-  } catch (error) {
-    console.log(error);
+  const contactsList = await listContacts();
+  const index = contactsList.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
   }
+  const [result] = contactsList.splice(index, 1);
+  await writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+  console.log(result);
 }
 
 // Add contacts - new array
 async function addContact(name, email, phone) {
-  try {
-    const objectContacts = {
-      id: nanoid(),
-      name: name,
-      email: email,
-      phone: phone,
-    };
+  const objectContacts = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
 
-    const contactsList = await listContacts();
-    const newArray = [...contactsList, objectContacts];
-    await writeFile(contactsPath, JSON.stringify(newArray, null, 2));
-    console.log(objectContacts);
-  } catch (error) {
-    console.log(error);
-  }
+  const contactsList = await listContacts();
+  const newArray = [...contactsList, objectContacts];
+  await writeFile(contactsPath, JSON.stringify(newArray, null, 2));
+  console.log(newArray);
 }
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
+  listContacts: wrapper(listContacts),
+  getContactById: wrapper(getContactById),
+  removeContact: wrapper(removeContact),
+  addContact: wrapper(addContact),
 };
